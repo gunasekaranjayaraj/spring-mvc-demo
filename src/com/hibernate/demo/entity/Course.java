f90c19aae493f7a37ee52e2a0d10f6093e8f337b
unknown <gunajai20@gmail.com>
2022-03-29 13:55:50 +0530

    one to many uni directional mapping

--- src/com/hibernate/demo/entity/Course.java
+++ src/com/hibernate/demo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,30 +20,34 @@ import javax.persistence.Table;
 public class Course {
 
 	// define our fields
-	
+
 	// define constructors
-	
+
 	// define getter setters
-	
+
 	// define tostring
-	
+
 	// annotate fields
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="id")
 	private int id;
-	
+
 	@Column(name="title")
 	private String title;
-	
+
 	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
-						 CascadeType.DETACH, CascadeType.REFRESH})
+			CascadeType.DETACH, CascadeType.REFRESH})
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
-	
+
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List<Review> reviews;
+
 	public Course() {
-		
+
 	}
 
 	public Course(String title) {
@@ -69,10 +78,29 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+	// add a convenience method
+
+	public void addReview(Review theReview) {
+
+		if (reviews == null) {
+			reviews = new ArrayList<>();
+		}
+
+		reviews.add(theReview);
+	}
+
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + "]";
 	}
-	
-	
+
+
 }
